9f1840ca8c0f2cd712ada65ea7ecdf8ebbdc3cfd
gkishanrao <kishangugul@gmail.com>
2022-10-20 17:46:06 -0400

    First Commit...

---
+++ src/main/java/com/example/demo/GitDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+		
+		System.out.println("Welcome to Git!");
+	}
+
+}
